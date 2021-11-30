2f814938cdbe125941ca25c05abdb899138fea46
Alex <thed4rkof@gmail.com>
2021-11-30 19:55:18 +0400

    add save_yaml to imports

--- warepy/__init__.py
+++ warepy/__init__.py
@@ -2,6 +2,6 @@ from .core.logger import logger
 from .core.singleton import Singleton 
 from .core.toolkit import (
     join_paths, dump_json_to_environ, load_json_from_environ,
-    load_yaml, format_message, get_next_dict_key,
+    load_yaml, save_yaml, format_message, get_next_dict_key,
     normalize_db_uri, get_or_error,
 )
