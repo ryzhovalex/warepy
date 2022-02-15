187ad07e54a67e59ec55831c7b52200475cd64a2
ryzhovalex <thed4rkof@gmail.com>
2022-02-15 16:24:09 +0400

    Fix init bugs

--- warepy/__init__.py
+++ warepy/__init__.py
@@ -3,5 +3,5 @@ from .core.singleton import Singleton
 from .core.toolkit import (
     join_paths, dump_json_to_environ, load_json_from_environ,
     load_yaml, save_yaml, format_message, get_next_dict_key,
-    normalize_db_uri, get_or_error, get_literal_args,
+    normalize_db_uri, get_or_error,
 )
--- warepy/core/singleton.py
+++ warepy/core/singleton.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 from abc import ABCMeta
+from typing import TypeVar
 
 
-class Singleton(type):
+SingletonInstance = TypeVar("SingletonInstance")
+
+
+class SingletonMeta(type):
     """Singleton metaclass for implementing singleton patterns. 
     Source: https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
 
@@ -17,4 +21,10 @@ class Singleton(type):
         if cls not in cls.instances:
             cls.instances[cls] = super().__call__(*args, **kwargs)
         return cls.instances[cls]
-        
+
+
+class Singleton(metaclass=SingletonMeta):
+    """Singleton base class."""
+    @classmethod
+    def instance(cls: type[SingletonInstance]) -> SingletonInstance:
+        return cls()
