b9dce2d3faac1fa6cd9802df7f7cf7e75837d836
ryzhovalex <thed4rkof@gmail.com>
2022-03-06 21:45:40 +0400

    Rename core to src

--- warepy/__init__.py
+++ warepy/__init__.py
@@ -1,6 +1,6 @@
-from .core.logger import logger
-from .core.singleton import Singleton 
-from .core import (
+from .src.logger import logger
+from .src.singleton import Singleton 
+from .src import (
     join_paths, dump_json_to_environ, load_json_from_environ,
     load_yaml, save_yaml, format_message, get_next_dict_key,
     normalize_db_uri, get_or_error, get_enum_values, extend_enum,
